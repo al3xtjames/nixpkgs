1ae637806e9363f96f2c23b0fce739d37f414ee6
Alex James <git@alextjam.es>
2025-10-05 21:18:32 -0500

    x265: build from Git & simplify CMake version generation

--- pkgs/by-name/x2/x265/package.nix
+++ pkgs/by-name/x2/x265/package.nix
@@ -2,11 +2,11 @@
   lib,
   gccStdenv,
   stdenv,
-  fetchurl,
+  fetchFromBitbucket,
+  fetchpatch2,
+  replaceVars,
   cmake,
   nasm,
-  fetchpatch,
-  fetchpatch2,
 
   # NUMA support enabled by default on NUMA platforms:
   numaSupport ? (
@@ -34,7 +34,13 @@ let
   isCross = stdenv.buildPlatform != stdenv.hostPlatform;
 in
 
-stdenv.mkDerivation (finalAttrs: {
+stdenv.mkDerivation (finalAttrs: let
+  versionParts = builtins.match "([0-9]\.[0-9])\\+?([0-9]+)?-?([0-9a-z]+)?" finalAttrs.version;
+  latestTag = lib.lists.elemAt versionParts 0;
+  orDefault = value: default: if value != null then value else default;
+  tagDistance = orDefault (lib.lists.elemAt versionParts 1) "0";
+  gitRev = orDefault (lib.lists.elemAt versionParts 2) latestTag;
+in {
   pname = "x265";
   version = "4.1";
 
@@ -43,11 +49,11 @@ stdenv.mkDerivation (finalAttrs: {
     "dev"
   ];
 
-  # Check that x265Version.txt contains the expected version number
-  # whether we fetch a source tarball or a tag from the git repo
-  src = fetchurl {
-    url = "https://bitbucket.org/multicoreware/x265_git/downloads/x265_${finalAttrs.version}.tar.gz";
-    hash = "sha256-oxaZxqiYBrdLAVHl5qffZd5LSQUEgv5ev4pDedevjyk=";
+  src = fetchFromBitbucket {
+    owner = "multicoreware";
+    repo = "x265_git";
+    rev = gitRev;
+    hash = "sha256-gv0H3qPeK473GbZiq2fxuIMQdBFjvoeMpyf5/Y5wlyI=";
   };
 
   patches = [
@@ -75,6 +81,11 @@ stdenv.mkDerivation (finalAttrs: {
       stripLen = 1;
       hash = "sha256-qEihgUKGEdthbKz67s+/hS/qdpzl+3tEB3gx2tarax4=";
     })
+    (replaceVars ./simplify-cmake-version.patch {
+      x265_version = finalAttrs.version;
+      x265_latest_tag = latestTag;
+      x265_tag_distance = tagDistance;
+    })
   ]
   # TODO: remove after update to version 4.2
   ++ lib.optionals (stdenv.hostPlatform.isAarch32 && stdenv.hostPlatform.isLinux) [
@@ -85,19 +96,7 @@ stdenv.mkDerivation (finalAttrs: {
     })
   ];
 
-  sourceRoot = "x265_${finalAttrs.version}/source";
-
-  postPatch = ''
-    substituteInPlace cmake/Version.cmake \
-      --replace-fail "unknown" "${finalAttrs.version}" \
-      --replace-fail "0.0" "${finalAttrs.version}"
-  ''
-  # There is broken and complicated logic when setting X265_LATEST_TAG for
-  # mingwW64 builds. This bypasses the logic by setting it at the end of the
-  # file
-  + lib.optionalString stdenv.hostPlatform.isMinGW ''
-    echo 'set(X265_LATEST_TAG "${finalAttrs.version}")' >> ./cmake/Version.cmake
-  '';
+  sourceRoot = "source/source";
 
   nativeBuildInputs = [
     cmake
@@ -204,7 +203,7 @@ stdenv.mkDerivation (finalAttrs: {
     mainProgram = "x265";
     homepage = "https://www.x265.org";
     changelog = "https://x265.readthedocs.io/en/master/releasenotes.html#version-${
-      lib.strings.replaceStrings [ "." ] [ "-" ] finalAttrs.version
+      lib.strings.replaceStrings [ "." ] [ "-" ] latestTag
     }";
     license = lib.licenses.gpl2Plus;
     maintainers = [ ];
