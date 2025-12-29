b5a937141349b22bfa040c16ef4a7007f1a4b949
Alex James <git@alextjam.es>
2025-12-29 16:15:50 -0600

    dash-mpd-cli: enable sandbox feature on Linux
    dash-mpd-cli 0.2.29 added experimental support for sandboxing using the
    Landlock LSM on Linux. By default dash-mpd-cli allows read and execute
    permissions for /lib, /usr, and any paths in $PATH and $LD_LIBRARY_PATH
    [1]. This does not work as-is on Nix as the dynamic libraries used by
    the runtime binaries (such as ffmpeg) do not live under /lib or /usr.
    Instead, patch the dash-mpd-rs crate to allow /nix in the sandbox.
    
    [1]: https://github.com/emarsden/dash-mpd-rs/blob/7d0ee20f5686a409e1c263c55fdd7832aca0c141/src/sandbox.rs#L44-L51

--- pkgs/by-name/da/dash-mpd-cli/package.nix
+++ pkgs/by-name/da/dash-mpd-cli/package.nix
@@ -4,18 +4,24 @@
   rustPlatform,
   fetchFromGitHub,
   makeWrapper,
-  bento4,
   protobuf,
+  bento4,
   ffmpeg,
   gpac,
   libxslt,
   shaka-packager,
   nix-update-script,
+  replaceVars,
   runCommand,
   versionCheckHook,
 }:
 
 let
+  # Patch the dash-mpd crate to allow access to the Nix store in the sandbox.
+  dash-mpd-patch = replaceVars ./dash-mpd-rs-nix-sandbox.patch {
+    storeDir = builtins.storeDir;
+  };
+
   # dash-mpd-cli looks for a binary named `shaka-packager`, while
   # shaka-packager provides `packager`.
   shaka-packager-wrapped = runCommand "shaka-packager-wrapped" { } ''
@@ -37,6 +43,12 @@ rustPlatform.buildRustPackage (finalAttrs: {
   cargoHash = "sha256-9HDpgff+JJY2qIo6Pl96c1wrTP0/j7ikgiG0lU4Nt88=";
 
   __structuredAttrs = true;
+
+  postPatch = ''
+    patch -d $cargoDepsCopy/*/dash-mpd-* -i ${dash-mpd-patch} -p 1
+  '';
+
+  buildFeatures = lib.optionals stdenvNoCC.hostPlatform.isLinux [ "sandbox" ];
 
   # Needed for HTTP3 support (which is enabled by default):
   # https://github.com/emarsden/dash-mpd-cli/blob/2d53fa0c077ede18c9cee198903a7884e880d47a/.github/workflows/ci.yml#L5-L7
