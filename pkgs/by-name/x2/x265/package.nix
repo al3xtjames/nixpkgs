925c9b681c2e79bca4b2fadd2bff2409cc5b01fd
Alex James <git@alextjam.es>
2026-06-08 02:27:55 -0500

    x265: use versionCheckHook

--- pkgs/by-name/x2/x265/package.nix
+++ pkgs/by-name/x2/x265/package.nix
@@ -28,6 +28,8 @@
   # NEON support is always enabled for aarch64
   # this flag is only needed for armv7.
   neonSupport ? false, # force enable the NEON fpu support for arm v7 CPUs
+
+  versionCheckHook,
 }:
 
 let
@@ -195,6 +197,10 @@ in {
   + lib.optionalString stdenv.hostPlatform.isMinGW ''
     ln -s $out/bin/*.dll $out/lib
   '';
+
+  doInstallCheck = true;
+
+  nativeInstallCheckInputs = [ versionCheckHook ];
 
   __structuredAttrs = true;
 
