418a274ccb6433be5a9277bffc444772fd390b71
Alex James <git@alextjam.es>
2026-07-11 00:14:42 -0500

    lix: cleanup System V IPC objects on Darwin

--- pkgs/tools/package-management/lix/default.nix
+++ pkgs/tools/package-management/lix/default.nix
@@ -236,6 +236,8 @@ lib.makeExtensible (
         patches = [
           lixFunctional2TimeoutPatch
           ./limit-optional-pkgconfig-deps.patch
+          ./0001-Fix-macOS-IPC-cleanup-using-sysctl-shared-memory-seg.patch
+          ./0002-libstore-cleanup-System-V-IPC-objects-after-aborted-.patch
         ];
       };
     };
