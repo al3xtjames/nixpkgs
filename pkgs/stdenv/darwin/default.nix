cb673f2399bc28923ee5c52586863ed55fe9bb04
Alex James <git@alextjam.es>
2026-07-13 23:42:17 -0700

    Revert "{stdenv,darwin.*,apple-sdk,ld64}: drop `x86_64-darwin` support (#535508)"
    This reverts commit 3bb703a955a60da4534c9918c2254f5634aca1b8, reversing
    changes made to d862c1367f87bf8464b75ba16192891c3d3e1077.

--- pkgs/stdenv/darwin/default.nix
+++ pkgs/stdenv/darwin/default.nix
@@ -282,6 +282,7 @@ let
   };
   sdkDarwinPackages = prevStage: {
     inherit (prevStage.darwin)
+      Csu
       adv_cmds
       copyfile
       libiconv
@@ -1055,6 +1056,7 @@ assert bootstrapTools.passthru.isFromBootstrapFiles or false; # sanity check
             prevStage.updateAutotoolsGnuConfigScriptsHook
             prevStage.updateAutotoolsGnuConfigScriptsHook.gnu_config
           ]
+          ++ lib.optionals localSystem.isx86_64 [ prevStage.darwin.Csu ]
           ++ (with prevStage.darwin; [
             binutils
             binutils.bintools
