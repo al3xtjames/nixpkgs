fc2fac196208d1e8ef9aff2ee1fe7a8134d2117f
Alex James <git@alextjam.es>
2026-07-25 15:02:24 -0700

    python3Packages.black: remove TMPDIR override on Darwin
    It doesn't seem like this is needed anymore. It also causes pytest's
    cleanup to fail [1].
    
    [1]: https://gist.github.com/al3xtjames/22ff498fddbd0429dfeac7de307acba5

--- pkgs/development/python-modules/black/default.nix
+++ pkgs/development/python-modules/black/default.nix
@@ -78,10 +78,6 @@ buildPythonPackage (finalAttrs: {
     # The top directory /build matches black's DEFAULT_EXCLUDE regex.
     # Make /build the project root for black tests to avoid excluding files.
     touch ../.git
-  ''
-  + lib.optionalString stdenv.hostPlatform.isDarwin ''
-    # Work around https://github.com/psf/black/issues/2105
-    export TMPDIR="/tmp"
   '';
 
   disabledTests = [
