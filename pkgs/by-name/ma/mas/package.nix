8a9453b7c2ab238a8181a501bc2861924f92d6dd
Alex James <git@alextjam.es>
2026-08-03 00:01:05 -0700

    Revert "mas: clean up after `x86_64-darwin` drop"
    This reverts commit 7ca063e9d128399edd96cf5902af70541e90e53b.

--- pkgs/by-name/ma/mas/package.nix
+++ pkgs/by-name/ma/mas/package.nix
@@ -16,11 +16,23 @@ stdenvNoCC.mkDerivation (finalAttrs: {
 
   __structuredAttrs = true;
 
-  # nix store prefetch-file https://github.com/mas-cli/mas/releases/download/v$VERSION/mas-$VERSION-arm64.pkg
-  src = fetchurl {
-    url = "https://github.com/mas-cli/mas/releases/download/v${finalAttrs.version}/mas-${finalAttrs.version}-arm64.pkg";
-    hash = "sha256-vCGKhUyF2eHJVJapayYoe7ZgVrlWiLkPkdBPpi7SG3U=";
-  };
+  src =
+    let
+      # nix store prefetch-file https://github.com/mas-cli/mas/releases/download/v$VERSION/mas-$VERSION-$ARCH.pkg
+      sources =
+        {
+          aarch64-darwin = {
+            arch = "arm64";
+            hash = "sha256-vCGKhUyF2eHJVJapayYoe7ZgVrlWiLkPkdBPpi7SG3U=";
+          };
+        }
+        .${stdenvNoCC.hostPlatform.system}
+          or (throw "Unsupported system: ${stdenvNoCC.hostPlatform.system}");
+    in
+    fetchurl {
+      url = "https://github.com/mas-cli/mas/releases/download/v${finalAttrs.version}/mas-${finalAttrs.version}-${sources.arch}.pkg";
+      inherit (sources) hash;
+    };
 
   nativeBuildInputs = [
     installShellFiles
